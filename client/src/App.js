e22666b655e9d5bf28fbc742fd90bb75a3a63f74
paragasR <r.paragas@outlook.com>
2022-03-15 12:03:54 -0600

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+    uri: '/graphql',
+  });
+  
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+  
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+  function App() {
+      return(
+          <ApolloProvider client={client}>
+              <Router>
+                  {/* add components here */}
+              </Router>
+          </ApolloProvider>
+      )
+  }
